8017d0742b7aaee9b3ae18e64736fd0b88f9fc64
Gabriele-Zini <gabrielezini845@gmail.com>
2023-10-27 18:14:32 +0200

    add: program logic

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 const container = document.querySelector(".container");
 const select = document.getElementById("level");
+const playBtn = document.getElementById("play-btn");
 
 select.addEventListener("change", handleSelect);
+playBtn.addEventListener("click", handlePlayBtn);
 
+// FUNZIONI
+
+// funzione per creare la griglia
 function createGrid(numMax) {
   for (let i = 1; i <= numMax; i++) {
     const cell = generateCell(numMax);
     cell.textContent = i;
-    cell.addEventListener("click", handleClick);
     container.append(cell);
   }
 }
 
-// FUNZIONI
+// funzione per creare una singola cella
 function generateCell(numMax) {
   const newCell = document.createElement("div");
   newCell.classList.add("cell");
@@ -20,20 +24,30 @@ function generateCell(numMax) {
   return newCell;
 }
 
+// funzione per gestire il click sulla singola cella
 function handleClick() {
   const innerNumber = parseInt(this.textContent);
   this.classList.toggle("light-blue");
   console.log(innerNumber);
 }
 
+// funzione per gestire il select
 function handleSelect() {
   let value = select.value;
   container.innerHTML = "";
   if (value === "easy") {
     createGrid(100);
+    container.classList.add("d-hidden");
   } else if (value === "normal") {
     createGrid(81);
+    container.classList.add("d-hidden");
   } else if (value === "hard") {
     createGrid(49);
+    container.classList.add("d-hidden");
   }
+}
+
+// funzione per gestire il tasto play
+function handlePlayBtn() {
+  container.classList.remove("d-hidden");
 }
